24bdd6a096e3fe36e49475ab9be2d1e9158aa888
ClaudioYe <jye64@uwo.ca>
2020-10-10 18:57:35 -0400

    change to the same random seed

--- decisionTree.py
+++ decisionTree.py
@@ -45,7 +45,7 @@ X = data_onehot.drop(['price'], axis=1)
 Y = data_onehot['price']
 
 # split training and test set
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=25)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # standard Scaler to fit training data in X
 scalerX = StandardScaler().fit(X_train)
--- randomForest.py
+++ randomForest.py
@@ -44,7 +44,7 @@ X = data_onehot.drop(['price'], axis=1)
 Y = data_onehot['price']
 
 # split training and test set
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # standard Scaler to fit training data in X
 scalerX = StandardScaler().fit(X_train)
